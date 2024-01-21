ed9ad4024353fb6db26b6b2cf4e2660ca55e8ae7
KevinDanudoro <kevindanudoro68@gmail.com>
2024-01-21 19:14:16 +0700

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 import type { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>Issues Page</div>;
+  return (
+    <div>
+      <Button>Create New Issue</Button>
+    </div>
+  );
 };
 
 export default page;
